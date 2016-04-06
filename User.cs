0ed156145454a37ce69663b5358653e0039a3923
utsgithub <uts_github@163.com>
2016-04-06 17:39:56 +1000

    Added files via upload
    FIRST FILE

---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class User
+    {
+        private int userType;
+
+        public User(int type)
+        {
+            userType = type;
+        }
+        public int getType()
+        {
+            //Leeke
+            return userType;
+        }
+    }
+}

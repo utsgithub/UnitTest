65c42d86051d5cb6f99f5456a017186531aa424e
BaoHD <kajck1994@gmail.com>
2016-04-06 17:41:16 +1000

    Added files via upload
    some of unit tests for stage transition

---
+++ UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        User siteEn;
+        User manager;
+        User account;
+        Intervention inter;
+        public TestContext TextContext { get; set; }
+        [TestInitialize]
+        public void Setup()
+        {
+            siteEn = new User(1);
+            manager = new User(2);
+            account = new User(3);
+            inter = new Intervention();
+        }
+        [TestMethod]
+        public void validateProposed_to_Approved()
+        {
+            Assert.AreEqual("Successful", inter.validate(2));
+        }
+        [TestMethod]
+        public void validateProposed_to_Completed()
+        {
+            Assert.AreEqual("Fail", inter.validate(0));
+        }
+        [TestMethod]
+        public void validateApproved_to_Cancelled()
+        {
+            inter.stageTransition(2);
+            Assert.AreEqual("Successful", inter.validate(0));
+        }
+        [TestMethod]
+        public void validateApproved_to_Completed()
+        {
+            inter.stageTransition(2);
+            Assert.AreEqual("Successful", inter.validate(0));
+        }
+        [TestMethod]
+        public void validateCancelled_to_Approved()
+        {
+            inter.stageTransition(1);
+            Assert.AreEqual("Successful", inter.validate(2));
+        }
+    }
+}
